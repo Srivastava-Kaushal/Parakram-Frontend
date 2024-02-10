ec66719a1b9f4451490252f18201a15e9b816149
Shrikrishna Mathur <22je0547@iitism.ac.in>
2024-02-10 15:28:09 +0530

    better foldet structre

--- src/components/FormComps/AddMembers.jsx
+++ src/components/FormComps/AddMembers.jsx
@@ -16,6 +16,7 @@ const AddMember = ({ index, push, remove, insert }) => {
         type="text"
         placeholder=""
       />
+
       <Button
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 secondary"
---
+++ src/pages/profileBuild.jsx
@@ -0,0 +1,11 @@
+import SignUpForm from "../components/ProfileFormComp/SignUpForm";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default ProfilePage;
