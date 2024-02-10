7eaf9b825e99f3f4ac52dde493f5393778128ea7
Yash Sharma <22je1104@iitism.ac.in>
2024-02-10 16:28:56 +0530

    Completed Events Page (without animation)

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,24 @@ import {
 import NavBar from './components/navbar/NavBar';
 import Landing from './pages/Landing';
 import Merchandise from './pages/Merchandise';
-import RegisterPage from "./pages/EventRegistration";
+import RegisterPage from './pages/EventRegistration';
 import EventsPage from './pages/EventsPage';
 
-
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route exact path="/" element={<NavBar />}>
-        <Route path="/" element={<Landing />} />
-        <Route path="/merchandise" element={<Merchandise />} />
-        <Route path="/register" element={<RegisterPage />} />
-		<Route path="/events" element={<EventsPage />} />
-		
-        {/* add component/pages here */}
-      </Route>
-    )
-  );
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route exact path="/" element={<NavBar />}>
+				<Route path="/" element={<Landing />} />
+				<Route path="/merchandise" element={<Merchandise />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/events" element={<EventsPage />} />
 
-  return <RouterProvider router={router} />;
+				{/* add component/pages here */}
+			</Route>,
+		),
+	);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/EventsPageComponent/SportBanner.jsx
+++ src/components/EventsPageComponent/SportBanner.jsx
@@ -1,15 +1,14 @@
-import FootballImg from '../../assets/Images/Football2.jpg';
 
 // Now I want to find proper image for the rest of the sports and populate individual sports too
 // make use of array for banner info
 // add hover effect =-> more backdrop and the slanted container shift animation
 
-const SportBanner = () => {
+const SportBanner = ({sportImg, sportName}) => {
     return (
-        <div className="h-[35vh] w-[80vw] bg-black my-10 rounded-2xl overflow-hidden relative shadow-[0_0_2px_1px_] shadow-[#727272] ">
-            <img src={FootballImg} alt="Football" className='w-full'/>
+        <div className="h-[35vh] w-[80vw] bg-black my-10 rounded-2xl overflow-hidden relative shadow-[0_0_4px_2px_] shadow-[#727272] ">
+            <img src={sportImg} alt="sport" className='w-full'/>
             <span className='w-[80vw] h-[120vh] bg-black/30 backdrop-blur-md absolute z-10 top-0 -left-96 rotate-45'>s</span>
-            <span className='text-white text-6xl absolute top-[50%] -translate-y-[50%] left-14 z-20'>Football</span>
+            <span className='text-white text-6xl absolute top-[50%] -translate-y-[50%] left-14 z-20'>{sportName}</span>
         </div>
     )
 }
