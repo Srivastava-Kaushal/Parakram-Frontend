90a0554b233b1120bef2c5834dfd2b4e94c28882
Yash Sharma <22je1104@iitism.ac.in>
2024-02-11 20:30:28 +0530

    Fixed navbar

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,8 +1,11 @@
 import transition from "../transition";
+import BgImage from "../assets/Images/AboutUsBg.avif"
 
 const AboutUs = () => {
     return (
-        <div className="min-h-screen"></div>
+        <div className="min-h-screen relative">
+            <img src={BgImage} alt="bgimage" className="w-full fixed top-0 -z-10"/>
+        </div>
     )
 }
 
