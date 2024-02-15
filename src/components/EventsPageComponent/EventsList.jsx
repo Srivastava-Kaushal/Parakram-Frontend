fedb007fa7f759bf265c7aade56a112e5c2021d7
Ratul <cs.ratul03@gmail.com>
2024-02-15 22:35:42 +0530

    g form for event reg

--- src/components/EventsPageComponent/EventsList.jsx
+++ src/components/EventsPageComponent/EventsList.jsx
@@ -1,40 +1,130 @@
 import SportBanner from "./SportBanner";
-import FootballImg from '../../assets/Images/Football2.jpg';
-import CricketImg from '../../assets/Images/Cricket.jpg'
-import HockeyImg from '../../assets/Images/Hockey.jpg'
-import VolleyballImg from '../../assets/Images/Volleyball.jpg'
-import BasketballImg from '../../assets/Images/Basketball.jpg'
-import KabaddiImg from '../../assets/Images/Kabaddi.jpg'
-import AthleticsImg from '../../assets/Images/ath.jpg';
-import BadmintonImg from '../../assets/Images/Badminton.jpg'
-import TableTennisImg from '../../assets/Images/TableTennis.jpg'
-import SquashImg from '../../assets/Images/Squash.jpg'
-import ChessImg from '../../assets/Images/Chess.jpg'
-import TennisImg from '../../assets/Images/Tennis.jpg'
-import PowerliftingImg from '../../assets/Images/Powerlifting.jpg'
-import BoxingImg from '../../assets/Images/Boxing.jpg'
-import KarateImg from '../../assets/Images/Karate.jpg'
+import FootballImg from "../../assets/Images/Football2.jpg";
+import CricketImg from "../../assets/Images/Cricket.jpg";
+import HockeyImg from "../../assets/Images/Hockey.jpg";
+import VolleyballImg from "../../assets/Images/Volleyball.jpg";
+import BasketballImg from "../../assets/Images/Basketball.jpg";
+import KabaddiImg from "../../assets/Images/Kabaddi.jpg";
+import AthleticsImg from "../../assets/Images/ath.jpg";
+import BadmintonImg from "../../assets/Images/Badminton.jpg";
+import TableTennisImg from "../../assets/Images/TableTennis.jpg";
+import SquashImg from "../../assets/Images/Squash.jpg";
+import ChessImg from "../../assets/Images/Chess.jpg";
+import TennisImg from "../../assets/Images/Tennis.jpg";
+import PowerliftingImg from "../../assets/Images/Powerlifting.jpg";
+import BoxingImg from "../../assets/Images/Boxing.jpg";
+import KarateImg from "../../assets/Images/Karate.jpg";
 
 const EventsList = () => {
-	return (
-		<div className='w-full flex flex-col items-center'>
-			<SportBanner sportImg={FootballImg} sportName={"Football"} eventLink={"/registerEvent/football"}/>
-			<SportBanner sportImg={CricketImg} sportName={"Cricket"} eventLink={"/registerEvent/cricket"}/>
-			<SportBanner sportImg={HockeyImg} sportName={"Hockey"} eventLink={"/registerEvent/hockey"}/>
-			<SportBanner sportImg={VolleyballImg} sportName={"Volleyball"} eventLink={"/registerEvent/volleyball"}/>
-			<SportBanner sportImg={BasketballImg} sportName={"Basketball"} eventLink={"/registerEvent/basketball"}/>
-			<SportBanner sportImg={KabaddiImg} sportName={"Kabaddi"} eventLink={"/registerEvent/kabaddi"}/>
-			<SportBanner sportImg={AthleticsImg} sportName={"Athletics"} eventLink={"/registerEvent/athletics"}/>
-            <SportBanner sportImg={BadmintonImg} sportName={"Badminton"} eventLink={"/registerEvent/badminton"}/>
-            <SportBanner sportImg={TableTennisImg} sportName={"Table Tennis"} eventLink={"/registerEvent/tabletennis"}/>
-            <SportBanner sportImg={SquashImg} sportName={"Squash"} eventLink={"/registerEvent/squash"}/>
-            <SportBanner sportImg={ChessImg} sportName={"Chess"} eventLink={"/registerEvent/chess"}/>
-            <SportBanner sportImg={TennisImg} sportName={"Tennis"} eventLink={"/registerEvent/tennis"}/>
-            <SportBanner sportImg={PowerliftingImg} sportName={"Power Lifting"} eventLink={"/registerEvent/powerlifting"}/>
-            <SportBanner sportImg={BoxingImg} sportName={"Boxing"} eventLink={"/registerEvent/boxing"}/>
-            <SportBanner sportImg={KarateImg} sportName={"Karate"} eventLink={"/registerEvent/karate"}/>
-		</div>
-	);
+  return (
+    <div className="w-full flex flex-col items-center">
+      <SportBanner
+        sportImg={FootballImg}
+        sportName={"Football"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLScN42V1fYMVrFD2I0hqnY76uhGpiJoDQRABQ8_IG9Wbz_W8pA/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={CricketImg}
+        sportName={"Cricket"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLScVKsYEMnR7VE-ZC7e7mTHzMFLGQB4HqOuczmOIALBwe-BEVw/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={HockeyImg}
+        sportName={"Hockey"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSeSi62rYGDleZi89O7n61xd4lwd64fet1xDVv9DvJwz_Tyz2w/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={VolleyballImg}
+        sportName={"Volleyball"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSesHa1BiLBuHtDSlEo0cEJiDnAsbuwH807c93ADUoTXRiU5LQ/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={BasketballImg}
+        sportName={"Basketball"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSeUGquMMbtStyObdJMPxbjt7gC39hFRkLwYXE9uA-dRlff8ZQ/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={KabaddiImg}
+        sportName={"Kabaddi"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSeCgmcsTFaro7z69PXK4w0dL1Qj_Jhlka2OojHbeGYL9m7BSw/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={AthleticsImg}
+        sportName={"Athletics"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSdqLLt_dR8l8-Q2bnhZcevaInGGVZCZstDueRYzHVHQ_xvW7A/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={BadmintonImg}
+        sportName={"Badminton"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSce9esKkLa-YaG_EKTkaM86T4txDl5XgmfEXfzXBD4mdPrOCg/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={TableTennisImg}
+        sportName={"Table Tennis"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSef-Pkrj25D03jrYd69au6SRBRBF62GSmbIkuZ2S_zJGdTXUA/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={SquashImg}
+        sportName={"Squash"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSd9cB8fvLIRcJl7dG0IkureJOVRWEj8yPAmLxjOsQqs8vmg2Q/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={ChessImg}
+        sportName={"Chess"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSfOx4CR6WztYcqdYEgoLn-QNoC0uJGF9FcN-3g5ynbv1mvngg/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={TennisImg}
+        sportName={"Tennis"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSc9voNSjZgR1cO6FlM2Wlfv7A8cbR3AZX4PEANqz7cJwadPvg/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={PowerliftingImg}
+        sportName={"Power Lifting"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSet_viNODmukxFJPB4GuCQIkwNdBhhRCkAMEUgLlP2DQOyLgw/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={BoxingImg}
+        sportName={"Boxing"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSfBDza7BRUioqFAn27POQm_PgkPERFj5TCVf1lbgcssCvDWXg/viewform?usp=sf_link"
+        }
+      />
+      <SportBanner
+        sportImg={KarateImg}
+        sportName={"Karate"}
+        eventLink={
+          "https://docs.google.com/forms/d/e/1FAIpQLSe9t9IYn77OmRDHrZIux0H6OJHN6aAVgUnSzIQ2N8f-swLqeg/viewform?usp=sf_link"
+        }
+      />
+    </div>
+  );
 };
 
 export default EventsList;
--- src/components/EventsPageComponent/SportBanner.jsx
+++ src/components/EventsPageComponent/SportBanner.jsx
@@ -1,73 +1,73 @@
-import { motion, useInView } from 'framer-motion';
-import { useRef, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './banner.css';
+import { motion, useInView } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./banner.css";
 // Now I want to find proper image for the rest of the sports and populate individual sports too
 // make use of array for banner info
 // add hover effect =-> more backdrop and the slanted container shift animation
 
 const bannerVariantsBig = {
-	hidden: { opacity: 0, x: 100, transition: { duration: 1, ease: 'easeOut' } },
-	show: { opacity: 1, x: 0, transition: { duration: 0.5, ease: 'easeOut' } },
+  hidden: { opacity: 0, x: 100, transition: { duration: 1, ease: "easeOut" } },
+  show: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
 };
 
 const bannerVariantsSmall = {
-	hidden: {
-		opacity: 0,
-		x: -10,
-		y: 100,
-		transition: { duration: 1, ease: 'easeOut' },
-	},
-	show: {
-		opacity: 1,
-		x: -10,
-		y: 0,
-		transition: { duration: 0.5, ease: 'easeOut' },
-	},
+  hidden: {
+    opacity: 0,
+    x: -10,
+    y: 100,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+  show: {
+    opacity: 1,
+    x: -10,
+    y: 0,
+    transition: { duration: 0.5, ease: "easeOut" },
+  },
 };
 
 const SportBanner = ({ sportImg, sportName, eventLink }) => {
-	const [isSmall, setIsSmall] = useState(false);
-	const ref = useRef(null);
-	const isInView = useInView(ref, {
-		threshold: 0.2,
-		triggerOnce: true,
-		amount: 0.5,
-		duration: 0.5,
-	});
-	useEffect(() => {
-		const { innerWidth } = window;
-		if (innerWidth <= 1000) {
-			setIsSmall(true);
-			console.log('Dem , small screen');
-		}
-	}, []);
+  const [isSmall, setIsSmall] = useState(false);
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    threshold: 0.2,
+    triggerOnce: true,
+    amount: 0.5,
+    duration: 0.5,
+  });
+  useEffect(() => {
+    const { innerWidth } = window;
+    if (innerWidth <= 1000) {
+      setIsSmall(true);
+      console.log("Dem , small screen");
+    }
+  }, []);
 
-	return (
-		<motion.div
-			ref={ref}
-			className="h-[35vh] w-[80vw] bg-black my-10 rounded-2xl overflow-hidden relative shadow-[0_0_4px_2px_] shadow-[#727272]"
-			initial="hidden"
-			animate={isInView ? 'show' : 'hidden'}
-			variants={isSmall ? bannerVariantsSmall : bannerVariantsBig}
-			whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-		>
-			<Link to={eventLink}>
-				{/* <img src={sportImg} alt="sport" className="w-full" /> */}
-				<img
-					src={sportImg}
-					alt="sport"
-					className="object-cover"
-					style={isSmall ? { height: '100%' } : { width: '100%' }}
-				/>
-				{/* <span className="w-[80vw] h-[120vh] bg-black/30 backdrop-blur-md absolute z-10 top-0 -left-96 rotate-45"> */}
-				<span className="blurry-div  bg-black/30 backdrop-blur-md absolute z-10 top-0 -left-96 rotate-45"></span>
-				<span className="text-[2.5rem] sm:text-[5rem] md:text-[7rem] lg:text-[5rem] text-white absolute top-[50%] -translate-y-[50%] left-14 z-20">
-					{sportName}
-				</span>
-			</Link>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      ref={ref}
+      className="h-[35vh] w-[80vw] bg-black my-10 rounded-2xl overflow-hidden relative shadow-[0_0_4px_2px_] shadow-[#727272]"
+      initial="hidden"
+      animate={isInView ? "show" : "hidden"}
+      variants={isSmall ? bannerVariantsSmall : bannerVariantsBig}
+      whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+    >
+      <Link to={eventLink} target="_blank">
+        {/* <img src={sportImg} alt="sport" className="w-full" /> */}
+        <img
+          src={sportImg}
+          alt="sport"
+          className="object-cover"
+          style={isSmall ? { height: "100%" } : { width: "100%" }}
+        />
+        {/* <span className="w-[80vw] h-[120vh] bg-black/30 backdrop-blur-md absolute z-10 top-0 -left-96 rotate-45"> */}
+        <span className="blurry-div  bg-black/30 backdrop-blur-md absolute z-10 top-0 -left-96 rotate-45"></span>
+        <span className="text-[2.5rem] sm:text-[5rem] md:text-[7rem] lg:text-[5rem] text-white absolute top-[50%] -translate-y-[50%] left-14 z-20">
+          {sportName}
+        </span>
+      </Link>
+    </motion.div>
+  );
 };
 
 export default SportBanner;
