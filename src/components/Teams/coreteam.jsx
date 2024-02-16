0cfb2a0afce83efc2d6178011b579f75144403c7
Ratul <cs.ratul03@gmail.com>
2024-02-16 12:52:31 +0530

    phone number

---
+++ src/components/Teams/coreteam.jsx
@@ -0,0 +1,261 @@
+const teams = [
+    {
+        coordinator:"Valluri Agasthya"
+    },
+    {
+        cocoordinator:[
+            {
+                name:"Bhupender Ghosliya",
+                image:""
+            },
+            {
+                name:"Yash Murarka",
+                image:""
+            }
+        ]
+    },
+    {
+        division: "Event Team",
+        head: [
+            {
+                name: "Kapil Yadav",
+                image: "https://drive.google.com/file/d/1pHP11R_wB5gmKfB1yQiyWCzlMOOOtJ1N/view?usp=drive_link"
+            },
+            {
+                name: "Akarapu Bhageerath",
+                image: "https://drive.google.com/file/d/1P1vpXCWkGYPJZL6irK8J78DLhcB0dLUc/view?usp=drive_link"
+            },
+            {
+                name: "Karthik Sri Ram Kumar",
+                image: "https://drive.google.com/file/d/1KGEryrB6jque0CfY3ry6GipBJJOPRBQi/view?usp=drive_link"
+            },
+            {
+                name: "SVSN Naidu",
+                image: ""
+            },
+        ],
+    },
+    {
+        division: "Hospitality Team",
+        head: [
+            {
+                name: "Guna Charan",
+                image: "https://drive.google.com/file/d/16Nx7p4kA2Gydyx8y7gBZ_p9ShqTBGZF0/view?usp=drive_link"
+            },
+            {
+                name: "Karri Sumanth Kumar",
+                image: "https://drive.google.com/file/d/17dGZbTgKidAEjkwNt-uprSjczsDf2a6p/view?usp=drive_link"
+            },
+            {
+                name: "Kushal Sharma",
+                image: "https://drive.google.com/file/d/1uzMxBOW00v-BdLwYT34IT9s7etpX6nxL/view?usp=drive_link"
+            },
+            {
+                name: "Sambhrant Dora",
+                image: ""
+            },
+            {
+                name: "VLD Devi",
+                image: ""
+            },
+        ],
+
+    },
+    {
+        division: "Sponsorship and Alumini Connect Team",
+        head: [
+            {
+                name: "Ganesula Yeshwanth",
+                image: "https://drive.google.com/file/d/1p7rgvKRLxvlJws8zwVX3zu3iDoQMl4Yw/view?usp=drive_link"
+            },
+            {
+                name: "Harsha Agarwal",
+                image: ""
+            },
+        ],
+
+    },
+    {
+        division: "Finance Team",
+        head: [
+            {
+                name: "T Raghu Vamshi Reddy",
+                image: "https://drive.google.com/file/d/1vTOcyuYUUmGnN-wiNsilf-nqWscbD-tq/view?usp=drive_link"
+            },
+        ],
+
+    },
+    {
+        division: "Operations and Logistics Team",
+        head: [
+            {
+                name: "Vishnu Teja",
+                image: "https://drive.google.com/file/d/1Ej5ENgpb8YIFUXBOGZzU3Ly_K5_znna5/view?usp=drive_link"
+            },
+            {
+                name: "Yellapu Jashwanth",
+                image: "https://drive.google.com/file/d/11ABKbLvELhZ0cMqF6UzGETVTYdwCTY9j/view?usp=drive_link"
+            },
+            {
+                name: "Abhishek Yadav",
+                image: "https://drive.google.com/file/d/1O4vfFGCReOYIyJJT9lIeI1SnAKd6oWXW/view?usp=drive_link"
+            },
+            {
+                name:"Sunil Kumar Rar",
+                image:"https://drive.google.com/file/d/1nLyk_nyPMsg_H56RCXqPr0P22QsGGEjJ/view?usp=drive_link"
+            }
+        ],
+    },
+    {
+        division: "Promotion and Marketing Team",
+        head: [
+            {
+                name: "Mukul Agarwal",
+                image: "https://drive.google.com/file/d/1AIv7c7NhobZHEqZoHjgLb0_86tSD3REi/view?usp=drive_link"
+            },
+            {
+                name: "Manish Kumar Jha",
+                image: "https://drive.google.com/file/d/12JTl6ndgzAlZDEYU5-mbACvzGzR6jaL4/view?usp=drive_link"
+            },
+        ],
+
+    },
+    {
+        division: "Security Team",
+        head: [
+            {
+                name: "T Lakshmi Ganeswar Reddy",
+                image: "https://drive.google.com/file/d/1O_8Cc4ncpvrjTbqQeQSc8lcxELIsasCS/view?usp=drive_link"
+            },
+            {
+                name: "Dhananjay Singh Shekhawat",
+                image: ""
+            },
+        ],
+
+    },
+    {
+        division: "Content and Documentation Team",
+        head: [
+            {
+                name: "K Vivek",
+                image: ""
+            },
+            {
+                name: "Disha Mewara",
+                image: ""
+            },
+        ],
+
+    },
+    {
+        division: "Design Team",
+        head: [
+            {
+                name: "Shubham Kumar Kurrey",
+                image: "https://drive.google.com/file/d/1nks4PnLlmIHEqHsy97n-eIBmQgwPep33/view?usp=drive_link"
+            },
+            {
+                name: "Bugatha Harshith Sai",
+                image: "https://drive.google.com/file/d/1g-nfC8ZdrljXS3WW-q_r97ZbJXe6ae3u/view?usp=drive_link"
+            },
+        ],
+
+    },
+    {
+        division: "Public Relations Team",
+        head: [
+            {
+                name: "Rounak Raj",
+                image: "https://drive.google.com/file/d/1FaAI0M7kpSPRCQiteYFGo1QoGL2wRDkR/view?usp=drive_link"
+            },
+            {
+                name: "Chavali Srinivas",
+                image: "https://drive.google.com/file/d/16XR5KS0ZYptNioFi1TFYAxJgHBtfHku-/view?usp=drive_link"
+            },
+            {
+                name: "Divyansh Sharma",
+                image: ""
+            },
+        ],
+
+    },
+    {
+        division: "Technical and Support Team",
+        head: [
+            {
+                name: "R S N Sasmith",
+                image: ""
+            },
+            {
+                name: "Pulipati Sai Nikhil",
+                image: ""
+            },
+            {
+                name: "Chirag Gajana",
+                image: "https://drive.google.com/file/d/16vMRgh_VVvt4BbsukDRd4PrSoRUY_8uw/view?usp=drive_link"
+            },
+            {
+                name: "Teella Roop Sagar",
+                image: "https://drive.google.com/file/d/1tmxoTHjWNBL4smWwrqtDQczrkqkSCvT0/view?usp=drive_link"
+            },
+        ],
+        supervisor: [
+            {
+                name: "Raghvan Pareek",
+                image: "https://drive.google.com/file/d/14x6PynFf6g94USmFlmUpf3CvOqoRxiCk/view?usp=drive_link"
+            },
+            {
+                name: "Drethi Manish Kapila",
+                image: ""
+            },
+            {
+                name: "Maniben Rathod",
+                image: "https://drive.google.com/file/d/13jdn8soTj8Mx646I9u4wsCqtVCFKPRIS/view?usp=drive_link"
+            },
+            {
+                name: "Kaushal Kumar Srivastava",
+                image: "https://drive.google.com/file/d/1ZRdwd21S9_OSRKXy6mk9Yi4OBO2zr1O5/view?usp=drive_link"
+            },
+            {
+                name: "Devineni Sri Venkatraya Chowdary",
+                image: ""
+            },
+            {
+                name: "Dharma Raghava Sai Shashank",
+                image: ""
+            },
+            {
+                name: "Pankaj Sharma",
+                image: ""
+            },
+        ],
+        organiser: [
+            {
+                name: "Ratul Saha",
+            },
+            {
+                name: "Harsh Singh",
+            },
+            {
+                name: "Prakhar Srivastava",
+            },
+            {
+                name: "Marmick Mathur",
+            },
+            {
+                name: "Yash Sharma",
+            },
+            {
+                name: "Ayush Barman",
+            },
+            {
+                name: "Subham Dey"
+            },
+        ]
+
+    },
+
+]
+
+export default teams;
