0b06d50087d4c5a615f80cd03df263443bfa3ffa
Ratul <cs.ratul03@gmail.com>
2024-02-16 16:15:20 +0530

    teams

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,7 +6,7 @@ function Team() {
   return (
     <div className="bg-[#1c100c]">
       <h1 className="pt-10 flex justify-center mb-8 text-4xl font-bold tracking-tight leading-none text-[#dad3a5] md:text-5xl lg:text-6xl">
-        Teams
+        Core Team
       </h1>
       <hr />
       <Teampage />
