e4f73c37c8222d961ec691697956259bc7d7aff1
Srivastava-Kaushal <21je0459@iitism.ac.in>
2024-03-08 00:10:47 +0530

    minor update

--- src/components/EventsPageComponent/EventsList.jsx
+++ src/components/EventsPageComponent/EventsList.jsx
@@ -22,7 +22,7 @@ const EventsList = () => {
         sportImg={FootballImg}
         sportName={"Football"}
         eventLink={
-          "https://drive.google.com/file/d/1-Vt8vnict1cA2bhczq2ZTfW8U8UIpic_/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLScN42V1fYMVrFD2I0hqnY76uhGpiJoDQRABQ8_IG9Wbz_W8pA/viewform?usp=sf_link"
         }
         scheduleLink={
           "https://drive.google.com/file/d/1BW5Qppk7vp9gVKum1gqUhY21x9XZdJqP/view?usp=drive_link"
@@ -32,7 +32,7 @@ const EventsList = () => {
         sportImg={CricketImg}
         sportName={"Cricket"}
         eventLink={
-          "https://drive.google.com/file/d/1pv1U7tPyUFb6kuCD7dlmC8E8BEu97LY7/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLScVKsYEMnR7VE-ZC7e7mTHzMFLGQB4HqOuczmOIALBwe-BEVw/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1AaEKscsLP6cwghdkNbQI7YhWTRKop96g/view?usp=drive_link"}
       />
@@ -47,14 +47,14 @@ const EventsList = () => {
         sportImg={VolleyballImg}
         sportName={"Volleyball"}
         eventLink={
-          "https://drive.google.com/file/d/15aKiY3xsE2i2Eqws4q2WROHON8MzOcwz/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSesHa1BiLBuHtDSlEo0cEJiDnAsbuwH807c93ADUoTXRiU5LQ/viewform?usp=sf_link"
         }
       />
       <SportBanner
         sportImg={BasketballImg}
         sportName={"Basketball"}
         eventLink={
-          "https://drive.google.com/file/d/1ZMm28V0Yzg7RxCYFNZY6VZ7LJ6PXgS-9/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSeUGquMMbtStyObdJMPxbjt7gC39hFRkLwYXE9uA-dRlff8ZQ/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/17sUO9TP5eNET1aejPr_mQWsOh7DVS_ph/view?usp=drive_link"}
       />
@@ -62,7 +62,7 @@ const EventsList = () => {
         sportImg={KabaddiImg}
         sportName={"Kabaddi"}
         eventLink={
-          "https://drive.google.com/file/d/1AOzZJDGLMl_HI-hB8o4ncr3QbXrUe6j7/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSeCgmcsTFaro7z69PXK4w0dL1Qj_Jhlka2OojHbeGYL9m7BSw/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1LEJgqHlyYiCIccDoE8tfRNJkp_Kt1AyS/view?usp=drive_link"}
       />
@@ -70,7 +70,7 @@ const EventsList = () => {
         sportImg={AthleticsImg}
         sportName={"Athletics"}
         eventLink={
-          "https://drive.google.com/file/d/1l17SAFfkQJMBzZxR8IETlgiK6VfeYCyd/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSdqLLt_dR8l8-Q2bnhZcevaInGGVZCZstDueRYzHVHQ_xvW7A/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1pXm1GMqFYtDJ0JjaaaScBXVpFKRvFoy1/view?usp=drive_link"}
       />
@@ -78,7 +78,7 @@ const EventsList = () => {
         sportImg={BadmintonImg}
         sportName={"Badminton"}
         eventLink={
-          "https://drive.google.com/file/d/1mm0XEqCuYd1kswOwpAael3Ra0jiBspqC/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSce9esKkLa-YaG_EKTkaM86T4txDl5XgmfEXfzXBD4mdPrOCg/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1spEw4ZMsmC-6u_oc8RaBbLPDwwfASK1K/view?usp=drive_link"}
       />
@@ -86,7 +86,7 @@ const EventsList = () => {
         sportImg={TableTennisImg}
         sportName={"Table Tennis"}
         eventLink={
-          "https://drive.google.com/file/d/1PAhnNHdaXHPaKcRsjbpCPLCdwFY8Kzfb/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSef-Pkrj25D03jrYd69au6SRBRBF62GSmbIkuZ2S_zJGdTXUA/viewform?usp=sf_link"
         }
         scheduleLink={
           "https://drive.google.com/file/d/1qvuVh8zD-r8r_F1x8pGXQ4cHOUobiLPz/view?usp=drive_link"
@@ -96,7 +96,7 @@ const EventsList = () => {
         sportImg={SquashImg}
         sportName={"Squash"}
         eventLink={
-          "https://drive.google.com/file/d/1hbdNFxBwzpDx3xdJSpKsSXu5ZfQT5KUU/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSd9cB8fvLIRcJl7dG0IkureJOVRWEj8yPAmLxjOsQqs8vmg2Q/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1qheeFAvEdlv19wcUqKUUkhniOexUbItq/view?usp=drive_link"}
       />
@@ -104,7 +104,7 @@ const EventsList = () => {
         sportImg={ChessImg}
         sportName={"Chess"}
         eventLink={
-          "https://drive.google.com/file/d/1steHeQnWfZ-V-EkVeNDqhUWihstQyUn-/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSfOx4CR6WztYcqdYEgoLn-QNoC0uJGF9FcN-3g5ynbv1mvngg/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1R5VTFR9-wO0MZxQJemlgFUL2xsw0nVVj/view?usp=drive_link"}
       />
@@ -112,7 +112,7 @@ const EventsList = () => {
         sportImg={TennisImg}
         sportName={"Tennis"}
         eventLink={
-          "https://drive.google.com/file/d/1F_BAZ74zQk9_X58tuEz2PzBWQPV9ronD/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSc9voNSjZgR1cO6FlM2Wlfv7A8cbR3AZX4PEANqz7cJwadPvg/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1VdKu1YpjkEhwp2B3GETs8ifBSbSy6cbb/view?usp=drive_link"}
       />
@@ -120,7 +120,7 @@ const EventsList = () => {
         sportImg={PowerliftingImg}
         sportName={"Power Lifting"}
         eventLink={
-          "https://drive.google.com/file/d/14E1cs_MNZTSEU8xHcCHJwKP7Bofao0Sy/view?usp=sharing"
+          "https://docs.google.com/forms/d/e/1FAIpQLSet_viNODmukxFJPB4GuCQIkwNdBhhRCkAMEUgLlP2DQOyLgw/viewform?usp=sf_link"
         }
         scheduleLink={"https://drive.google.com/file/d/1W2oq_cub9rna3zsyPfxGdm7Lg_yw3Llk/view?usp=drive_link"}
       />
